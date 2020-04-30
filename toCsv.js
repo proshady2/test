135f1fab58c9eeca716ece20bd365a0013cb1e9b
Ali Beheshtian <alibeheshtian.co@gmail.com>
2020-05-01 02:19:13 +0430

    sss

--- toCsv.js
+++ toCsv.js
@@ -1,41 +1,52 @@
 const https = require('request-promise');
 const ObjectsToCsv = require('objects-to-csv')
-
+const axios = require('axios');
 const urls = [];
 
-for (let i = 1; i < 46010; i++)
+for (let i = 1; i < 1000; i++)
     urls.push(`${i}.json`);
 
 
 
-(async () => {
-    const feeds = [];
+// (async () => {
+const feeds = [];
 
-    for (const url of urls) {
-        const options = {
-            'method': 'GET',
-            'url': 'https://player.fm/series/' + url + '?episode_detail=full',
-            'headers': {
-                'Referer': 'https://player.fm/series/the-invisible-man-by-h-g-wells',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-            },
-            'maxRedirects': 20,
-        };
-        await https(options).then(r => {
+for (const url of urls) {
+    // const options = {
+    //     'method': 'GET',
+    //     'url': 'https://player.fm/series/' + url,
+    //     'headers': {
+    //         'Referer': 'https://player.fm/series/the-invisible-man-by-h-g-wells',
+    //         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+    //     },
+    //     'maxRedirects': 20,
+    // };
+
+    axios.get('https://player.fm/series/' + url)
+        .then(function (response) {
+            // handle success
             console.log(url);
-
-
-
-            const dataObjects =
-            {
-                url: JSON.parse(r).url,
-                title: JSON.parse(r).title,
-            }
-
-            feeds.push(dataObjects);
-        }).catch(e => {
         })
-    }
-    const csv = new ObjectsToCsv(feeds)
-    await csv.toDisk('./list.csv')
-})();
+        .catch(function (error) {
+            // handle error
+            // console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+    // https(options).then(r => {
+    //     console.log(url);
+    //     // const dataObjects =
+    //     // {
+    //     //     url: JSON.parse(r).url,
+    //     //     title: JSON.parse(r).title,
+    //     // }
+
+    //     // feeds.push(dataObjects);
+    // }).catch(e => {
+    //     console.error(url);
+    // })
+}
+// const csv = new ObjectsToCsv(feeds)
+// await csv.toDisk('./list.csv')
+// })();
